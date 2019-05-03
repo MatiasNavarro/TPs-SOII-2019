2847b8e5d861151bc2174a190de4132dd6357824
Matias Navarro <matutenavarro95@gmail.com>
2019-05-02 21:01:21 -0300

    Update 1.1

--- TP1-SOII-2019/src/inet/cliente_i_cc.c
+++ TP1-SOII-2019/src/inet/cliente_i_cc.c
@@ -288,7 +288,7 @@ void updateFirmware(int sockfd, char *argv[])
 	fflush(stdout);
 	sleep(1);
 	close(sockfd);
-	system("cd /home/matiasnavarro/Facultad/2019/Sistemas_Operativos_II/Practicos/TPs-SOII-2019/TP1-SOII-2019/src/inet && make");
+	system("cd /home/matiasnavarro/Facultad/2019/Sistemas_Operativos_II/Practicos/TPs-SOII-2019/TP1-SOII-2019/src/inet && make update");
 	execvp(argv[0],argv);
 }
 
