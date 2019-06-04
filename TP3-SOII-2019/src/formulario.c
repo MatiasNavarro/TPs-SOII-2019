df6b892cd738a108a4d253bd1cb075c28aab591c
Matias Navarro <matutenavarro95@gmail.com>
2019-06-04 10:53:35 -0300

    Formulario

---
+++ TP3-SOII-2019/src/formulario.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h> 
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <dirent.h>
+#include <sys/sysinfo.h>
+
+
+int main(int argc, char* argv[]) {
+char* readInput = strchr(getenv("QUERY_STRING"),'=')+1;
+
+char *inputParse = strdup(readInput);
+char anio[100]={0};
+char dia[100]={0};
+
+strcat(anio,inputParse);
+strcat(dia,inputParse);
+
+char *year=strtok(dia,"&");
+printf("El listado solicitado del anio %s",year);
+
+char *day=strtok(NULL," ");
+day=strtok(day,"=");
+day=strtok(NULL," ");
+printf(" y dia %s es:\n",day);
+sleep(2);
+
+char path[100]={0};
+strcpy(path,"aws s3 --no-sign-request ls --recursive noaa-goes16/ABI-L2-CMIPF/");
+strcat(path,year);
+strcat(path,"/");
+strcat(path,day);
+strcat(path,"/");
+strcat(path," | grep M3C13");
+system(path);
+
+return 0;
+}
